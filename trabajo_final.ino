d0938db3ded9b37d4334842a348f1a879b2b0f6a
Jose Santiago Molano <jose.molano@upb.edu.co>
2023-11-08 17:13:16 -0500

    Funciona SD

--- trabajo_final.ino
+++ trabajo_final.ino
@@ -1,16 +1,19 @@
-const int pinPotentiometer = A0; // The analog pin where the potentiometer is connected
-int mappedValue = 0; // Variable to store the mapped value
+const int pinPotentiometer = A0;  // The analog pin where the potentiometer is connected
+int mappedValue = 0;              // Variable to store the mapped value
 int START_BUTTON = 4;
 int STOP_BUTTON = 3;
+int SHUTDOWN_PIN = 5;
 bool isStartButtonPressed = false;
 bool isStopButtonPressed = false;
 bool runProgram = false;
 bool programRunning = false;
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication at 9600 baud
+  Serial.begin(9600);  // Initialize serial communication at 9600 baud
   pinMode(START_BUTTON, INPUT_PULLUP);
   pinMode(STOP_BUTTON, INPUT_PULLUP);
+  pinMode(SHUTDOWN_PIN, OUTPUT);
+  digitalWrite(SHUTDOWN_PIN,LOW);
 }
 
 void loop() {
@@ -18,9 +21,11 @@ void loop() {
   isStopButtonPressed = readButton(STOP_BUTTON);
 
   if (isStartButtonPressed && !isStopButtonPressed && !programRunning) {
+    digitalWrite(SHUTDOWN_PIN,LOW);
     programRunning = true;
     runProgram = true;
   } else if (isStopButtonPressed) {
+    digitalWrite(SHUTDOWN_PIN,HIGH);
     programRunning = false;
     runProgram = false;
   }
@@ -29,11 +34,11 @@ void loop() {
     mapPotentiometer();
   }
 
-  delay(100); // Wait for a short period of time before reading the potentiometer again
+  delay(100);  // Wait for a short period of time before reading the potentiometer again
 }
 
 void mapPotentiometer() {
-  int potentiometerValue = analogRead(pinPotentiometer); // Read the potentiometer value (0-1023)
+  int potentiometerValue = analogRead(pinPotentiometer);  // Read the potentiometer value (0-1023)
 
   // Map the potentiometer value to the range of 30 to 90
   mappedValue = map(potentiometerValue, 0, 1023, 30, 90);
